3c947a455c805ecff043d0c40d9eb04221e6cfb9
Geon <quso12358@naver.com>
2023-10-24 17:10:41 +0900

    :heavy_plus_sign: Feat: Update Repo in Badge domain

--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/badge/repo/BadgeRepository.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/badge/repo/BadgeRepository.java
@@ -1,5 +1,13 @@
 package com.shield.dangdangranger.domain.badge.repo;
 
-public interface BadgeRepository {
+import com.shield.dangdangranger.domain.badge.entity.UserBadge;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface BadgeRepository extends JpaRepository<UserBadge, Integer> {
+    List<UserBadge> findAllByUserNoAndCanceled(Integer userNo, Integer canceled);
+
+    Optional<UserBadge> findUserBadgeByUserBadgeNoAndCanceled(Integer userBadgeNo, Integer canceled);
 }
