bddf0d93e714a68b5ef69d571be2b28d53685acd
Geon <quso12358@naver.com>
2023-10-24 15:25:51 +0900

    :heavy_plus_sign: Feat: Update Dto and Entity in Dog domain

--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/dog/dto/DogResponseDto.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/dog/dto/DogResponseDto.java
@@ -1,5 +1,34 @@
 package com.shield.dangdangranger.domain.dog.dto;
 
-public class DogResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+import java.time.LocalDateTime;
+
+public class DogResponseDto {
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class DogListResponseDto{
+        private Integer dogNo;
+        private String dogName;
+        private String dogBreed;
+        private Character dogSex;
+        private Integer dogTokenId;
+        private String dogImg;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class DogDetailResponseDto{
+        private Integer dogNo;
+        private String dogName;
+        private String dogBreed;
+        private Character dogSex;
+        private LocalDateTime dogBirth;
+        private Integer dogTokenId;
+        private String dogImg;
+    }
 }
--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/dog/entity/Dog.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/dog/entity/Dog.java
@@ -1,5 +1,50 @@
 package com.shield.dangdangranger.domain.dog.entity;
 
-public class Dog {
 
+import com.shield.dangdangranger.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "dogs")
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dog_no")
+    private Integer dogNo;
+
+    @Column(name = "user_no")
+    private Integer userNo;
+
+    @Column(name = "dog_name")
+    private String dogName;
+
+    @Column(name = "dog_breed")
+    private String dogBreed;
+
+    @Column(name = "dog_sex")
+    private Character dogSex;
+
+    @Column(name = "dog_birth")
+    private LocalDateTime dogBirth;
+
+    @Column(name = "dog_nose_print")
+    private String dogNosePrint;
+
+    @Column(name = "dog_token_id")
+    private Integer dogTokenId;
+
+    @Column(name = "dog_img")
+    private String dogImg;
+
+    @Column(name = "dog_hash")
+    private String dogHash;
 }
