09f2f8d6ed2643bc62d581348840b956c9459c6a
kimmainsain <kmsid003@gmail.com>
2023-10-25 14:44:22 +0900

    :construction_worker: Setting(Android) : com.dangdangranger -> com.anift.animaid 폴더구조변경, 패키지명 com.anift.animaid

--- front/android/app/src/main/java/com/anift/animaid/MainApplication.kt
+++ front/android/app/src/main/java/com/anift/animaid/MainApplication.kt
@@ -1,6 +1,7 @@
-package com.dangdangranger
+package com.anift.animaid
 
 import android.app.Application
+import com.dangdangranger.ReactNativeFlipper
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
