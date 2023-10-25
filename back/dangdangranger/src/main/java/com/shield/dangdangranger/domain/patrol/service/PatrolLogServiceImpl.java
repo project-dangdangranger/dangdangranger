8a25792cf42e2c0881fce1b0b742dc27bbe3621b
lkk <rabbit1999k@naver.com>
2023-10-25 14:42:56 +0900

    :heavy_plus_sign: Feat(patrol): 순찰 로그 등록 dong 관련 설정 추가

--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/patrol/service/PatrolLogServiceImpl.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/patrol/service/PatrolLogServiceImpl.java
@@ -1,16 +1,20 @@
 package com.shield.dangdangranger.domain.patrol.service;
 
 import static com.shield.dangdangranger.domain.patrol.constant.PatrolWritten.NOT_WRITTEN;
+import static com.shield.dangdangranger.domain.region.constant.RegionErrorMessage.DONG_NOT_FOUND_EXCEPTION;
 import static com.shield.dangdangranger.domain.user.constant.UserExceptionMessage.USER_NOT_FOUND_EXCEPTION;
 import static com.shield.dangdangranger.global.constant.BaseConstant.NOTCANCELED;
 
-import com.shield.dangdangranger.domain.common.DongCode;
-import com.shield.dangdangranger.domain.patrol.dto.PatrolRequestDto.PatrolLogRequestDto;
+import com.shield.dangdangranger.domain.patrol.dto.PatrolLogRequestDto.PatrolLogSaveRequestDto;
+import com.shield.dangdangranger.domain.patrol.dto.PatrolLogResponseDto.PatrolLogRoughInfoResponseDto;
 import com.shield.dangdangranger.domain.patrol.entity.PatrolLog;
 import com.shield.dangdangranger.domain.patrol.repo.PatrolLogRepository;
+import com.shield.dangdangranger.domain.region.entity.Dong;
+import com.shield.dangdangranger.domain.region.repo.DongRepository;
 import com.shield.dangdangranger.domain.user.entity.User;
 import com.shield.dangdangranger.domain.user.repo.UserRepository;
 import com.shield.dangdangranger.global.error.NotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,21 +26,30 @@ public class PatrolLogServiceImpl implements PatrolLogService {
 
     private final PatrolLogRepository patrolLogRepository;
     private final UserRepository userRepository;
+    private final DongRepository dongRepository;
 
     @Override
-    public void createPatrolLog(Integer userNo, PatrolLogRequestDto patrolLogRequestDto) {
+    public void createPatrolLog(Integer userNo, PatrolLogSaveRequestDto patrolLogSaveRequestDto) {
         User user = userRepository.findUserByUserNoAndCanceled(userNo, NOTCANCELED)
             .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_EXCEPTION.message()));
-
-        // TODO : dong code 설정
+        Dong dong = dongRepository.findDongByDongCodeAndCanceled(patrolLogSaveRequestDto.getDong(), NOTCANCELED)
+            .orElseThrow(() -> new NotFoundException(DONG_NOT_FOUND_EXCEPTION.message()));
 
         patrolLogRepository.save(PatrolLog.builder()
-            .patrolLogDate(patrolLogRequestDto.getPatrolLogDate())
-            .patrolRecordTotalTime(patrolLogRequestDto.getPatrolLogTotalTime())
-            .patrolLogLat(patrolLogRequestDto.getPatrolLogLat())
-            .patrolLogLng(patrolLogRequestDto.getPatrolLogLng())
-            .patrolLogImageUrl(patrolLogRequestDto.getPatrolLogImageUrl())
+            .user(user)
+            .dong(dong)
+            .patrolLogDate(patrolLogSaveRequestDto.getPatrolLogDate())
+            .patrolLogTotalDistance(patrolLogSaveRequestDto.getPatrolLogTotalDistance())
+            .patrolLogTotalTime(patrolLogSaveRequestDto.getPatrolLogTotalTime())
+            .patrolLogImageUrl(patrolLogSaveRequestDto.getPatrolLogImageUrl())
+            .patrolLogLat(patrolLogSaveRequestDto.getPatrolLogLat())
+            .patrolLogLng(patrolLogSaveRequestDto.getPatrolLogLng())
             .patrolWritten(NOT_WRITTEN.value())
             .build());
+    }
+
+    @Override
+    public List<PatrolLogRoughInfoResponseDto> readAllPatrolLog() {
+        return null;
     }
 }
