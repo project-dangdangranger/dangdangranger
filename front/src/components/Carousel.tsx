2f208bc043f4de4d76bc4d3b96c16550977604ba
Geon <quso12358@naver.com>
2023-10-27 15:44:05 +0900

    :art: Design: Update 4 button in Patrol page
    - 4 buttone icon img 추가
    - 4 button design 변경

--- front/src/components/Carousel.tsx
+++ front/src/components/Carousel.tsx
@@ -1,1 +1,73 @@
+import React, { useRef, useState } from "react";
+import { View, ScrollView, Text, Dimensions, StyleSheet } from "react-native";
 
+const Carousel = () => {
+	const [activeIndex, setActiveIndex] = useState(0);
+	const scrollViewRef = useRef(null);
+	const windowWidth = Dimensions.get("window").width;
+
+	const data = ["Slide 1", "Slide 2", "Slide 3"];
+
+	const handleScroll = (event: any) => {
+		const scrollPosition = event.nativeEvent.contentOffset.x;
+		const activeIndex = Math.floor(scrollPosition / windowWidth);
+		setActiveIndex(activeIndex);
+	};
+
+	return (
+		<View style={styles.container}>
+			<ScrollView
+				horizontal
+				pagingEnabled
+				showsHorizontalScrollIndicator={false}
+				onScroll={handleScroll}
+				scrollEventThrottle={16}
+				ref={scrollViewRef}
+			>
+				{data.map((item, index) => (
+					<View style={[styles.slide, { width: windowWidth }]} key={index}>
+						<Text>{item}</Text>
+					</View>
+				))}
+			</ScrollView>
+			<View style={styles.pagination}>
+				{data.map((_, index) => (
+					<View
+						key={index}
+						style={[styles.dot, index === activeIndex ? styles.activeDot : {}]}
+					/>
+				))}
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	slide: {
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "lightgray",
+		height: 200,
+	},
+	pagination: {
+		flexDirection: "row",
+		position: "absolute",
+		bottom: 10,
+		alignSelf: "center",
+	},
+	dot: {
+		width: 8,
+		height: 8,
+		borderRadius: 4,
+		backgroundColor: "gray",
+		marginHorizontal: 4,
+	},
+	activeDot: {
+		backgroundColor: "blue",
+	},
+});
+
+export default Carousel;
--- front/src/screens/PatrolMain.tsx
+++ front/src/screens/PatrolMain.tsx
@@ -18,6 +18,7 @@ import TempProfileImg from "../../assets/images/dog1.jpg";
 import WhitePenIcon from "../../assets/images/pen-icon.png";
 import FourBtn from "../recycles/FourBtn";
 import AbsoluteVar from "../recycles/FooterBar";
+import Carousel from "../components/Carousel";
 
 const PatrolMain = () => {
 	return (
@@ -26,41 +27,7 @@ const PatrolMain = () => {
 				<ColorHeader title="순찰" />
 				<View>
 					<View style={AlbumLayout.profileWrap}>
-						<Image
-							// source={{ uri: myProfileImg }}
-							source={TempProfileImg}
-							style={AlbumLayout.userPhoto}
-						/>
-						<TouchableOpacity
-							activeOpacity={0.7}
-							style={AlbumLayout.changeImageWrap}
-						>
-							<View>
-								<Image
-									source={WhitePenIcon}
-									style={AlbumLayout.changeImageIcon}
-								/>
-							</View>
-						</TouchableOpacity>
-					</View>
-					<View style={AlbumLayout.userColcontainer}>
-						<View style={AlbumLayout.userContainer}>
-							<Text style={AlbumLayout.userContainerText}>
-								"{"사용자의 닉네임"}" 님,
-							</Text>
-							<TouchableOpacity style={AlbumLayout.btnCSS1}>
-								<Text style={AlbumLayout.userContainerText1}>정보 검색</Text>
-							</TouchableOpacity>
-						</View>
-						<View style={AlbumLayout.userContainer}>
-							<Text style={AlbumLayout.userContainerText2}>
-								width: responsiveWidth(70), height: responsiveHeight(6), 최대
-								30글자
-							</Text>
-						</View>
-						<View style={AlbumLayout.DividSection}>
-							<View style={AlbumLayout.userSectionDivid}></View>
-						</View>
+						<Carousel></Carousel>
 					</View>
 				</View>
 				<FourBtn />
