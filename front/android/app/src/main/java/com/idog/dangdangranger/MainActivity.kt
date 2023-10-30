8673f414b220d6f5d115175621cbe87b0a36d8c5
kimmainsain <kmsid003@gmail.com>
2023-10-30 16:13:26 +0900

    :white_check_mark: Test : 카카오맵 연동 테스트

--- front/android/app/src/main/java/com/idog/dangdangranger/MainActivity.kt
+++ front/android/app/src/main/java/com/idog/dangdangranger/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.idog.dangdangranger
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import net.daum.mf.map.api.MapView
 
 class MainActivity : ReactActivity() {
+    private lateinit var mapView: MapView
+
     override fun getMainComponentName(): String? {
         return "댕댕레인저"
     }
 
-    override fun createReactActivityDelegate(): ReactActivityDelegate {
-        return DefaultReactActivityDelegate(
-            this,
-            mainComponentName!!,  // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-            fabricEnabled
-        )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
     }
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return DefaultReactActivityDelegate(this, mainComponentName!!)
+    }
+
 }
