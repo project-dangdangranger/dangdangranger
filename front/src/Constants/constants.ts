09fc6e46ffde4cf7322d916d4f01bb82ecbe790f
kimmainsain <kmsid003@gmail.com>
2023-10-31 14:14:38 +0900

    :white_check_mark: Test(Login) : encry에 토큰저장 및 콘솔출력 테스트

---
+++ front/src/Constants/constants.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://animaid.co.kr/api";
+export const CONTENT_TYPE = "application/json; charset=utf-8";
+export const TIMEOUT = 1000;
--- front/src/components/GoogleLogin.tsx
+++ front/src/components/GoogleLogin.tsx
@@ -2,7 +2,9 @@ import React, { useEffect } from "react";
 import { Button } from "react-native";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { WEB_CLIENT_ID } from "@env";
-
+import axios from "axios";
+import EncryptedStorage from "react-native-encrypted-storage";
+import { BASE_URL, CONTENT_TYPE, TIMEOUT } from "../Constants/constants";
 const GoogleLogin = () => {
 	useEffect(() => {
 		GoogleSignin.configure({
@@ -14,8 +16,33 @@ const GoogleLogin = () => {
 	const handleGoogleLogin = async () => {
 		try {
 			await GoogleSignin.hasPlayServices();
-			const userInfo = await GoogleSignin.signIn();
-			console.log(userInfo.idToken);
+			const responseCheckSigninData = await GoogleSignin.signIn();
+			const url = `${BASE_URL}/user`;
+			const responseCheckUserInfo = await axios.post(url, null, {
+				headers: {
+					Authorization: `Bearer ${responseCheckSigninData.idToken}`,
+					"Content-Type": CONTENT_TYPE,
+				},
+				timeout: TIMEOUT,
+			});
+			console.log("responseCheckUserInfo.data: ", responseCheckUserInfo.data);
+			console.log(
+				"responseCheckUserInfo.data.data.signInUp: ",
+				responseCheckUserInfo.data.data.signInUp,
+			);
+			await EncryptedStorage.setItem(
+				"accessToken",
+				responseCheckUserInfo.data.data.tokenInfo.accessToken,
+			);
+			await EncryptedStorage.setItem(
+				"refreshToken",
+				responseCheckUserInfo.data.data.tokenInfo.refreshToken,
+			);
+			// excryptedStorage에 저장된 토큰을 확인
+			const accessToken = await EncryptedStorage.getItem("accessToken");
+			const refreshToken = await EncryptedStorage.getItem("refreshToken");
+			console.log("accessToken: ", accessToken);
+			console.log("refreshToken: ", refreshToken);
 		} catch (error) {
 			console.error(error);
 		}
