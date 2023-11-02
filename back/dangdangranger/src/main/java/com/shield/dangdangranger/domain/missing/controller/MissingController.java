2b31fc17859abd147dd0d1ac8b152257ce797cc2
Donggyeom <eastcrtsy@gmail.com>
2023-11-02 17:15:53 +0900

    :heavy_plus_sign: Feat(missing): 실종견 등록 api

--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/missing/controller/MissingController.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/missing/controller/MissingController.java
@@ -1,17 +1,19 @@
 package com.shield.dangdangranger.domain.missing.controller;
 
-import static com.shield.dangdangranger.domain.missing.constant.MissingResponseMessage.GET_LOCAL_MISSING_DOG_COUNT_SUCCESS;
+import static com.shield.dangdangranger.domain.missing.constant.MissingResponseMessage.GET_LOCAL_MISSING_COUNT_SUCCESS;
+import static com.shield.dangdangranger.domain.missing.constant.MissingResponseMessage.CREATE_MISSING_SUCCESS;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shield.dangdangranger.domain.missing.dto.MissingRequestDto.MissingSaveRequestDto;
 import com.shield.dangdangranger.domain.missing.service.MissingService;
-import com.shield.dangdangranger.domain.patrol.controller.PatrolReportController;
-import com.shield.dangdangranger.domain.patrol.service.PatrolReportService;
 import com.shield.dangdangranger.global.dto.ResponseDto;
 
 import lombok.RequiredArgsConstructor;
@@ -28,6 +30,16 @@ public class MissingController {
 	@GetMapping("/count")
 	public ResponseEntity<ResponseDto<Integer>> getLocalMissingDogCount(@RequestAttribute("userNo") Integer userNo) {
 		return ResponseEntity.status(HttpStatus.OK).body(ResponseDto
-				.create(GET_LOCAL_MISSING_DOG_COUNT_SUCCESS.message(), missingService.getLocalMissingDogCount(userNo)));
+				.create(GET_LOCAL_MISSING_COUNT_SUCCESS.message(), missingService.getLocalMissingDogCount(userNo)));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<ResponseDto<String>> createMissing(
+			@RequestAttribute("userNo") Integer userNo,
+			@RequestBody MissingSaveRequestDto missingSaveRequestDto) {
+		
+		missingService.registMissing(userNo, missingSaveRequestDto);
+		return ResponseEntity.status(HttpStatus.OK).body(ResponseDto
+				.create(CREATE_MISSING_SUCCESS.message()));
 	}
 }
