d28a52af01fd292ae496e8769b63281e51599459
seobstation <ljs8828@naver.com>
2023-11-05 00:40:10 +0900

    :heavy_plus_sign: Feat: 순찰일지 내동네, 내일지 API 연결, 최신순 삭제

--- front/src/components/PatrolDiaryCard.tsx
+++ front/src/components/PatrolDiaryCard.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import {
 	responsiveHeight,
 	responsiveWidth,
@@ -9,25 +9,31 @@ import locationIcon from "../../assets/images/location-pin-icon.png";
 const PatrolDiaryCard = ({ imgSrc, patrolDiaryInfo }: any) => {
 	return (
 		<>
-			<View style={style.cardWrap}>
-				<View style={style.imgWrap}>
-					<Image source={imgSrc} style={style.img} />
+			<TouchableOpacity>
+				<View style={style.cardWrap}>
+					<View style={style.imgWrap}>
+						<Image source={imgSrc} style={style.img} />
+					</View>
+					<View style={style.textWrap}>
+						<View style={style.textLocationWrap}>
+							<Image source={locationIcon} style={style.iconLocation} />
+							<Text style={style.textLocation}>{patrolDiaryInfo.location}</Text>
+						</View>
+						<View style={style.textTitleWrap}>
+							<Text style={style.textTitle}>{patrolDiaryInfo.patrolTitle}</Text>
+						</View>
+						<View style={style.textRowWrap}>
+							<Text style={style.textRowWrapName}>
+								{patrolDiaryInfo.userName}
+							</Text>
+							<Text style={style.textRowWrapViewCount}>
+								{patrolDiaryInfo.patrolHit}
+							</Text>
+							<Text>{patrolDiaryInfo.patrolDate.slice(0, 10)}</Text>
+						</View>
+					</View>
 				</View>
-				<View style={style.textWrap}>
-					<View style={style.textLocationWrap}>
-						<Image source={locationIcon} style={style.iconLocation} />
-						<Text style={style.textLocation}>위치</Text>
-					</View>
-					<View style={style.textTitleWrap}>
-						<Text style={style.textTitle}>순찰 제목</Text>
-					</View>
-					<View style={style.textRowWrap}>
-						<Text>작성자</Text>
-						<Text>조회수</Text>
-						<Text>날짜</Text>
-					</View>
-				</View>
-			</View>
+			</TouchableOpacity>
 		</>
 	);
 };
@@ -37,13 +43,13 @@ export default PatrolDiaryCard;
 const style = StyleSheet.create({
 	cardWrap: {
 		marginHorizontal: responsiveWidth(2),
+		marginVertical: responsiveHeight(1),
 		width: responsiveWidth(40),
 		height: responsiveHeight(30),
-		borderRadius: 5,
+		borderRadius: 20,
 		borderColor: "#D3D3D3",
 		borderWidth: 1,
 		alignItems: "center",
-		//elevation: 1,
 	},
 
 	imgWrap: {},
@@ -74,7 +80,9 @@ const style = StyleSheet.create({
 	textTitleWrap: {
 		paddingBottom: responsiveHeight(0.5),
 	},
+
 	textTitle: {
+		height: 22,
 		fontWeight: "900",
 		fontSize: 20,
 		color: "black",
@@ -85,8 +93,12 @@ const style = StyleSheet.create({
 		justifyContent: "space-between",
 	},
 
-	debug: {
-		borderWidth: 2,
-		backgroundColor: "red",
+	textRowWrapName: {
+		width: responsiveWidth(13),
+		height: 15,
+	},
+	textRowWrapViewCount: {
+		width: responsiveWidth(6),
+		height: 15,
 	},
 });
