2761727a78992687496f656738668c48c96ec943
seobstation <ljs8828@naver.com>
2023-11-05 11:41:43 +0900

    :heavy_plus_sign: Feat: 순찰 내동네, 내일지 불러오기, 순찰 일지 작성  API 연결 완료

--- front/src/screens/PatrolDiary.tsx
+++ front/src/screens/PatrolDiary.tsx
@@ -1,10 +1,12 @@
+import { useRef } from "react";
 import {
 	Image,
-	ScrollView,
 	Text,
 	TextInput,
 	TouchableOpacity,
 	View,
+	Modal,
+	StyleSheet,
 } from "react-native";
 import ColorHeader from "../recycles/ColorHeader";
 import CommonLayout from "../recycles/CommonLayout";
@@ -19,19 +21,13 @@ import { useEffect, useState } from "react";
 import PatrolDiaryCard from "../components/PatrolDiaryCard";
 import dotIconImg from "../../assets/images/3-dot-icon.png";
 import axios from "../utils/axios";
+import { responsiveHeight } from "react-native-responsive-dimensions";
 
 const PatrolDiary = () => {
 	const { navigate } = useNavigation<StackNavigation>();
-	const [selectedOption, setSelectedOption] = useState("최신순");
-	const patrolDiaryInfo = {
-		nameid: "나노나누",
-		location: "서울특별시 역삼동",
-		title: "찾아주세요..제발..ㅇㅇㅇㅇㅇㅇㅇㅇ",
-		writer: "나노나누",
-		viewCount: "232",
-		date: "2023.01.01",
-	};
+	const [selectedOption, setSelectedOption] = useState("내동네");
 	const [patrolDiaryList, setPatrolDiaryList] = useState([]);
+	const [modalVisible, setModalVisible] = useState(false);
 
 	useEffect(() => {
 		if (selectedOption === "내일지") {
@@ -61,6 +57,14 @@ const PatrolDiary = () => {
 		}
 	}, [selectedOption]);
 
+	const [isMenuVisible, setMenuVisible] = useState(false);
+	const anchorRef = useRef();
+
+	const menuOptions = [
+		{ label: "글 작성하기", onPress: () => console.log("글 작성하기") },
+		// 여기에 더 많은 옵션을 추가할 수 있습니다.
+	];
+
 	return (
 		<>
 			<CommonLayout>
@@ -87,7 +91,14 @@ const PatrolDiary = () => {
 									selected={selectedOption === "내일지"}
 									onPress={() => setSelectedOption("내일지")}
 								/>
-								<TouchableOpacity onPress={() => navigate("PatrolDiaryWrite")}>
+
+								<TouchableOpacity
+									style={PatrolDiaryLayout.settings}
+									onPress={() => {
+										setModalVisible(true);
+										// navigate("PatrolDiaryWrite");
+									}}
+								>
 									<Image
 										source={dotIconImg}
 										style={PatrolDiaryLayout.iconImg}
@@ -95,31 +106,99 @@ const PatrolDiary = () => {
 								</TouchableOpacity>
 							</View>
 						</View>
-						<TextInput
-							style={PatrolDiaryLayout.formInput}
-							onChangeText={() => {}}
-							placeholder="실종견의 정보를 입력해주세요."
-						/>
+						<View>
+							<TextInput
+								style={PatrolDiaryLayout.formInput}
+								onChangeText={() => {}}
+								placeholder="실종견의 정보를 입력해주세요."
+							/>
+						</View>
 					</View>
+
 					<View style={PatrolDiaryLayout.patrolRowWrap}>
 						{patrolDiaryList?.map((patrolDiary) => {
 							return (
-								<PatrolDiaryCard
-									imgSrc={{ uri: patrolDiary.patrolFirstImg }}
-									patrolDiaryInfo={patrolDiary}
-									key={patrolDiary.patrolNo}
-								/>
+								<>
+									<PatrolDiaryCard
+										imgSrc={{ uri: patrolDiary.patrolFirstImg }}
+										patrolDiaryInfo={patrolDiary}
+										key={patrolDiary.patrolNo}
+									/>
+								</>
 							);
 						})}
 					</View>
 				</View>
-				<TouchableOpacity onPress={() => navigate("PatrolDiaryWrite")}>
-					<Text>Hi</Text>
-				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => navigate("PatrolDiaryWrite")}
+				></TouchableOpacity>
 			</CommonLayout>
 			<FooterBar />
+			<Modal
+				animationType="fade"
+				transparent={true}
+				visible={modalVisible}
+				onRequestClose={() => setModalVisible(false)}
+			>
+				<TouchableOpacity
+					style={PatrolDiaryLayout.modalContainer}
+					onPress={() => setModalVisible(false)}
+				>
+					<View style={PatrolDiaryLayout.modalView}>
+						<TouchableOpacity
+							style={PatrolDiaryLayout.modalItem}
+							onPress={() => {
+								navigate("PatrolDiaryWrite");
+								setModalVisible(false);
+							}}
+						>
+							<Text style={PatrolDiaryLayout.modalText}>글 작성하기</Text>
+						</TouchableOpacity>
+					</View>
+				</TouchableOpacity>
+			</Modal>
 		</>
 	);
 };
 
 export default PatrolDiary;
+
+const styles = StyleSheet.create({
+	container: {
+		position: "absolute",
+		bottom: 100,
+	},
+	button: {
+		padding: 10,
+		backgroundColor: "#DDDDDD",
+	},
+	buttonText: {
+		fontSize: 24,
+	},
+	modalContainer: {
+		flex: 1,
+		top: responsiveHeight(26),
+	},
+	modalView: {
+		backgroundColor: "white",
+		borderRadius: 20,
+		padding: 35,
+		alignItems: "flex-start",
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+	},
+	modalItem: {
+		// marginBottom: 15,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	modalText: {
+		fontSize: 10,
+	},
+});
--- front/src/screens/PatrolDiaryWrite.tsx
+++ front/src/screens/PatrolDiaryWrite.tsx
@@ -1,4 +1,11 @@
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+	Image,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+	Alert,
+} from "react-native";
 import ColorHeader from "../recycles/ColorHeader";
 import CommonLayout from "../recycles/CommonLayout";
 import FooterBar from "../recycles/FooterBar";
@@ -8,8 +15,13 @@ import img from "../../assets/images/debug-dog.png";
 import PatrolLogCarousel from "../recycles/PatrolLogCarousel";
 import AddPlusIcon from "../../assets/images/add-plus-icon.png";
 import CustomSubButton from "../recycles/CustomSubBtn";
+import { useState } from "react";
+import axios from "../utils/axios";
+import { useNavigation } from "@react-navigation/native";
 
 const PatrolDiaryWrite = () => {
+	const navigation = useNavigation();
+
 	const logs = [
 		{ logNo: 0, imgSrc: img, date: "22-02-02" },
 		{ logNo: 1, imgSrc: img, date: "22-02-02" },
@@ -17,6 +29,35 @@ const PatrolDiaryWrite = () => {
 		{ logNo: 3, imgSrc: img, date: "22-02-02" },
 		{ logNo: 4, imgSrc: img, date: "22-02-02" },
 	];
+
+	const [patrolReportTitle, setPatrolReportTitle] = useState("");
+	const [patrolReportContent, setPatrolReportContent] = useState("");
+	const [patrolReportImageList, setPatrolReportImageList] = useState([
+		"https://dangdangranger.s3.ap-northeast-2.amazonaws.com/map_2023-11-03T07%3A02%3A19.564Z_1072956.png",
+	]);
+	const [patrolLogNo, setPatrolLogNo] = useState(1);
+
+	const submitPatrolReport = () => {
+		axios
+			.post("/patrol", {
+				patrolReportTitle: patrolReportTitle,
+				patrolReportContent: patrolReportContent,
+				patrolReportImageList: patrolReportImageList,
+				patrolLogNo: patrolLogNo,
+			})
+			.then((res) => {
+				console.log("성공:", res.data);
+				if (res.data.message === "순찰일지 등록 성공") {
+					Alert.alert("순찰일지 등록 성공", "순찰 일지 화면으로 이동합니다.");
+
+					navigation.navigate("PatrolDiary");
+				}
+			})
+			.catch((err) => {
+				console.log("에러;", err);
+			});
+	};
+
 	return (
 		<>
 			<CommonLayout>
@@ -46,8 +87,8 @@ const PatrolDiaryWrite = () => {
 						</TouchableOpacity>
 						<TextInput
 							style={PatrolDiaryWriteLayout.formInput}
-							// value={petSpecies || ""}
-							onChangeText={() => {}}
+							value={patrolReportTitle}
+							onChangeText={(text) => setPatrolReportTitle(text)}
 							placeholder="제목을 작성해주세요."
 							onBlur={() => {}}
 						/>
@@ -62,18 +103,18 @@ const PatrolDiaryWrite = () => {
 								PatrolDiaryWriteLayout.formInput,
 								PatrolDiaryWriteLayout.textDesc,
 							]}
-							// value={petSpecies || ""}
-							onChangeText={() => {}}
+							value={patrolReportContent}
+							onChangeText={(text) => {
+								setPatrolReportContent(text);
+							}}
 							placeholder="순찰 일지 내용을 작성해주세요."
 							onBlur={() => {}}
 						/>
 						<CustomSubButton
 							text={"순찰일지 기록하기"}
-							onPress={
-								() => {}
-
-								// navigation.navigate("MakeDogProfile")
-							}
+							onPress={() => {
+								submitPatrolReport();
+							}}
 							color={"#70C8EE"}
 						/>
 					</View>
