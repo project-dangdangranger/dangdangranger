874f65ebc00292a8bd571182613f9b38a67e46ed
Donggyeom <eastcrtsy@gmail.com>
2023-11-08 16:06:52 +0900

    :heavy_plus_sign: Feat(missing): 실종견 리스트 조회

--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/missing/constant/MissingResponseMessage.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/missing/constant/MissingResponseMessage.java
@@ -3,7 +3,8 @@ package com.shield.dangdangranger.domain.missing.constant;
 public enum MissingResponseMessage {
 	
 	GET_LOCAL_MISSING_COUNT_SUCCESS("우리동네 실종견 수 조회 완료"),
-	CREATE_MISSING_SUCCESS("실종견(신고) 등록 성공");
+	CREATE_MISSING_SUCCESS("실종견(신고) 등록 성공"),
+	READ_ALL_MISSING("실종견 정보 리스트 조회 성공");
 	
 	private final String message;
 
--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/missing/repo/MissingRepository.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/missing/repo/MissingRepository.java
@@ -4,10 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shield.dangdangranger.domain.missing.entity.Missing;
 import com.shield.dangdangranger.domain.missing.repo.custom.MissingRepositoryCustom;
+
+import java.util.List;
 import java.util.Optional;
 
 
 public interface MissingRepository extends JpaRepository<Missing, Integer>, MissingRepositoryCustom  {
 
 	Optional<Missing> findByMissingNoAndCanceled(Integer missingNo, int canceled);
+	
+	List<Missing> findByMissingStatusAndCanceled(Integer missingStatus, int canceled);
 }
