8138129d932a84b579fe2cb68e26eed5a3308fd2
Donggyeom <eastcrtsy@gmail.com>
2023-11-13 13:57:16 +0900

    :heavy_plus_sign: Feat(finddog): 함께 찾기 참여자 수 조회 API (수정)

--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/missing/controller/FinddogController.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/missing/controller/FinddogController.java
@@ -2,6 +2,7 @@ package com.shield.dangdangranger.domain.missing.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.shield.dangdangranger.domain.missing.constant.FinddogResponseMessage;
 import com.shield.dangdangranger.domain.missing.dto.FinddogRequestDto.FinddogCreateRequestDto;
+import com.shield.dangdangranger.domain.missing.dto.FinddogResponseDto.FinddogParticipantsCountResponseDto;
 import com.shield.dangdangranger.domain.missing.service.FinddogService;
 import com.shield.dangdangranger.global.dto.ResponseDto;
 
@@ -41,4 +43,19 @@ public class FinddogController {
 				);
 	}
 
+	@GetMapping("/participants_count")
+	public ResponseEntity<ResponseDto<FinddogParticipantsCountResponseDto>> getFinddogParticipantsCount() {
+		
+		return ResponseEntity
+				.status(HttpStatus.CREATED)
+				.body(
+					ResponseDto.create(
+						FinddogResponseMessage.GET_FINDDOG_PARTICIPANTS_COUNT_SUCCESS.message(), 
+						FinddogParticipantsCountResponseDto
+							.builder()
+							.participantsCount(finddogService.getFinddogParticipants())
+							.build()
+					)
+				);
+	}
 }
