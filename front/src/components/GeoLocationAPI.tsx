c038719d85f2ea4ba68d0970675a3ab9e58dede5
seobstation <ljs8828@naver.com>
2023-11-14 13:27:52 +0900

    :bug: Fix: 등록 위치정보 수정

--- front/src/components/GeoLocationAPI.tsx
+++ front/src/components/GeoLocationAPI.tsx
@@ -3,11 +3,12 @@ import { Button, Text, View } from "react-native";
 import Geolocation from "@react-native-community/geolocation";
 import axios from "axios";
 import { GEOCODING_API_KEY } from "@env";
+import { Dispatch, SetStateAction } from "react";
 
 type Props = {
 	setAddress: (address: string) => void;
-	setMissinglat: (address: string) => void;
-	setMissinglong: (address: string) => void;
+	setMissinglat: Dispatch<SetStateAction<number>>;
+	setMissinglong: Dispatch<SetStateAction<number>>;
 };
 
 const GeoLocationAPI = (props: Props) => {
@@ -21,8 +22,14 @@ const GeoLocationAPI = (props: Props) => {
 				const latitude = JSON.stringify(position.coords.latitude);
 				const longitude = JSON.stringify(position.coords.longitude);
 				getAddressCode(Number(latitude), Number(longitude));
-				props.setMissinglat(Number(latitude));
-				props.setMissinglong(Number(longitude));
+				props.setMissinglat(Number(latitude).toFixed(3));
+				props.setMissinglong(Number(longitude).toFixed(3));
+				console.log(
+					Number(latitude).toFixed(3),
+					"++++++",
+					Number(longitude).toFixed(3),
+				);
+				console.log("플보스!ㅁ나ㅜ임이ㅓ", props);
 			},
 			(error) => {
 				console.log(error.code, error.message);
