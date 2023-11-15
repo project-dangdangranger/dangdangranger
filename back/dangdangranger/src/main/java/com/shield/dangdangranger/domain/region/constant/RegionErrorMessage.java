d5c73404bd41ceaf7c2f66cda7f33973e4b410a1
lkk <rabbit1999k@naver.com>
2023-11-15 10:06:29 +0900

    :hammer: Refactor(patrol): 순찰 기록 등록 방식 수정
    - postal address 동이름 -> 동코드로 변경

--- back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/region/constant/RegionErrorMessage.java
+++ back/dangdangranger/src/main/java/com/shield/dangdangranger/domain/region/constant/RegionErrorMessage.java
@@ -1,7 +1,8 @@
 package com.shield.dangdangranger.domain.region.constant;
 
 public enum RegionErrorMessage {
-    DONG_NOT_FOUND_EXCEPTION("동 코드가 존재하지 않습니다.")
+    DONG_NOT_FOUND_EXCEPTION("동 코드가 존재하지 않습니다."),
+    POSTAL_ADDRESS_NOT_FOUND_EXCEPTION("우편번호가 존재하지 않습니다.")
     ;
 
     String message;
